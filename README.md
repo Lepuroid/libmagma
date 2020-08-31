91a3ec20dcfd705f3b7635dd67d4cb9f7d42e432
Lepuroid <lepuroid@gmail.com>
2020-08-31 15:22:43 +0300

    +t_permut(), minor

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # libmagma
- magma cypher library
+ [GOST R 34.12-2015 (ru)](https://pdf.standartgost.ru/catalog/Data2/1/4293762/4293762704.pdf) Magma block cypher library in Rust.
--- src/lib.rs
+++ src/lib.rs
@@ -28,11 +28,20 @@ mod tests {
         println!("aa => {}\nbb => {}", aa, bb);
         let cc: Block = aa + bb;
         let mut dd: Block = cc;
-        dd += bb ;
+        dd += bb + Block::from_u32(256);
         println!("cc => {}\ndd => {}\n", cc, dd);
     }
     #[test]
     fn test_3() {
+        use crate::Block;
+        use crate::Cypher;
+
+        let aa: Block = Block::from_u32(0b_10100101001111000101101011000011_u32);
+        println!("{}", aa);
+        let _bb = aa.t_permut();
+    }
+    #[test]
+    fn test_4() {
         let mut path_in = r"C:\OneDrive\Projects\Rust\headache\log.csv";
         let key = r#"12345678"#;
         crate::encrypt_ecb(path_in, key);
@@ -51,8 +60,16 @@ use std::{
 
 const BLOCK_LEN: usize = 4;
 const BLOCK_LEN_INC: usize = BLOCK_LEN - 1;
+static TABLE: [[u8; 16]; 8] = [[1,7,14,13,0,5,8,3,4,15,10,6,9,12,11,2],
+                                [8,14,2,5,6,9,1,12,15,4,11,0,13,10,3,7],
+                                [5,13,15,6,9,2,12,10,11,7,8,1,4,3,14,0],
+                                [7,15,5,10,8,1,6,13,0,9,3,14,11,4,2,12],
+                                [12,8,2,1,13,4,15,6,7,0,10,5,3,14,9,11],
+                                [11,3,5,8,2,15,10,13,14,1,7,4,12,9,6,0],
+                                [6,8,2,3,9,10,5,12,1,14,4,7,11,13,0,15],
+                                [12,4,6,2,10,5,11,9,14,8,13,7,0,3,15,1]];
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Default)]
 struct Block([u8; BLOCK_LEN]);
 
 struct BlockIntoIterator {
@@ -102,15 +119,9 @@ impl IndexMut<usize> for Block {
     }
 }
 
-impl Default for Block {
-    fn default() -> Block {
-        Block { 0: [0; BLOCK_LEN] }
-    }
-}
-
 impl Display for Block {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(f, "[{:08b}, {:08b}, {:08b}, {:08b}]", self[0], self[1], self[2], self[3])
+        write!(f, "[{:08b} {:08b} {:08b} {:08b}]", self[0], self[1], self[2], self[3])
     }
 }
 
@@ -122,7 +133,7 @@ impl BitXorAssign for Block {
 
 impl BitXor for Block {
     type Output = Self;
-    fn bitxor(self, Block(rhs): Self) -> Self {
+    fn bitxor(self, Block(rhs): Self) -> Self::Output {
         let mut block: Block = Default::default();
         block.iter_mut().zip(self.iter().zip(rhs.iter())).for_each(|(c, (a, b))| *c = a ^ b);
         block
@@ -131,16 +142,15 @@ impl BitXor for Block {
 
 impl AddAssign for Block {
     fn add_assign(&mut self, rhs: Self) {
-        *self = Block::from_u32(u32::saturating_add(self.to_u32(), rhs.to_u32()))
+        *self = Block::from_u32(u32::wrapping_add(self.to_u32(), rhs.to_u32()))
     }
 }
 
 impl Add for Block {
     type Output = Self;
-    fn add(self, rhs: Self) -> Self {
-        Block::from_u32(u32::saturating_add(self.to_u32(), rhs.to_u32()))
+    fn add(self, rhs: Self) -> Self::Output {
+        Block::from_u32(u32::wrapping_add(self.to_u32(), rhs.to_u32()))
     }
-
 }
 
 trait Cypher {
@@ -149,13 +159,11 @@ trait Cypher {
     fn to_u32(&self) -> u32;
 
     fn from_array(array: [u8; BLOCK_LEN]) -> Block {
-        let block = Block { 0: array };
-        block
+        Block { 0: array }
     }
 
     fn from_u32(n: u32) -> Block {
-        let block: Block = Block::from_array(n.to_be_bytes());
-        block
+        Block::from_array(n.to_be_bytes())
     }
 }
 
@@ -170,6 +178,28 @@ impl Cypher for Block {
 
     fn to_u32(&self) -> u32 {
         u32::from_be_bytes(self.0)
+    }
+}
+
+impl Block {
+    fn t_permut(&self) -> Block {
+        let mask = 0b_11110000_u8;
+        let mut block: Block = Default::default();
+        let mut string_1: String = String::new();
+        let mut string_2: String = String::new();
+        let mut string_3: String = String::new();
+        for i in 0..BLOCK_LEN {
+            let l4 = (&self[i] & mask) >> 4;
+            let r4 = &self[i] & !mask;
+            string_1.push_str(&format!("    {:x}{:x}   ", l4, r4));
+            string_2.push_str(&format!("    ↓↓   "));
+            let l4_new = TABLE[i * 2][l4 as usize];
+            let r4_new = TABLE[i * 2 + 1][r4 as usize];
+            string_3.push_str(&format!("    {:x}{:x}   ", l4_new, r4_new));
+            block[i] = (l4_new << 4) + r4_new;
+        }
+        println!("{}\n{}\n{}\n{}", string_1, string_2, string_3, block);
+        block
     }
 }
 
